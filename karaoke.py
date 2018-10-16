96e3e747ad37ed0062d5109493a33898785b23d2
Julia Ortiz Naharro <jortizna@alpha15.aulas.gsyc.urjc.es>
2018-10-16 19:18:29 +0200

    karaoke.py hasta el apartado 6

--- karaoke.py
+++ karaoke.py
@@ -6,6 +6,7 @@ from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSmilHandler
 import sys
 import json
+from urllib.request import urlretrieve
 
 
 def printlines(Nlista):
@@ -17,11 +18,21 @@ def printlines(Nlista):
             if atributo[valor] != '':
                etiqueta +=  "\\t" + valor +"=\"" + atributo[valor] + "\""
         archiv_aux = archiv_aux + etiqueta + "\n"
-    print (archiv_aux[:-1])  # para salvar la linea en blanco, mirar como hacerlo mejor
+    print (archiv_aux,end='')  # para salvar la linea en blanco, mirar como hacerlo mejor
 
 def makejson(Nlista,fichero):
     json.dump(Nlista, open(fichero, "w"))
 
+def dowloand(Nlista):
+    url=""
+    for linea in Nlista:
+        atributo = linea[1]
+        for valor in atributo:
+            if valor == 'src'and atributo[valor].startswith('http://'):
+                url= atributo[valor]
+                name=atributo[valor].split("/")[-1]
+                urlretrieve(url,name)
+        #print (url) para ver lo que me descarga
 
 if __name__ == "__main__":
 
@@ -34,6 +45,7 @@ if __name__ == "__main__":
         parser.parse(open(fich))
         printlines(Nclase.get_tags())
         makejson(Nclase.get_tags(),fichjson)
+        dowloand(Nclase.get_tags())
 
     except IndexError:
-        sys.exit("sgdvb")
+        sys.exit("Usage: python3 karaoke.py file.smil.")
