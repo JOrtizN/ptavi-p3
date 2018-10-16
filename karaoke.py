2f240a0196ddab5524ba1b282d31c51ce4b227d6
Julia Ortiz Naharro <jortizna@alpha15.aulas.gsyc.urjc.es>
2018-10-16 17:39:50 +0200

    karaoke con json

--- karaoke.py
+++ karaoke.py
@@ -5,6 +5,7 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSmilHandler
 import sys
+import json
 
 
 def printlines(Nlista):
@@ -18,17 +19,21 @@ def printlines(Nlista):
         archiv_aux = archiv_aux + etiqueta + "\n"
     print (archiv_aux[:-1])  # para salvar la linea en blanco, mirar como hacerlo mejor
 
-#def makejson(Nlista,)
+def makejson(Nlista,fichero):
+    json.dump(Nlista, open(fichero, "w"))
 
 
 if __name__ == "__main__":
 
     try:
+        fich = (sys.argv[1])
+        fichjson = (sys.argv[1].replace(".smil", ".json"))
         parser = make_parser()
         Nclase = SmallSmilHandler()
         parser.setContentHandler(Nclase)
-        parser.parse(open(sys.argv[1]))
+        parser.parse(open(fich))
         printlines(Nclase.get_tags())
+        makejson(Nclase.get_tags(),fichjson)
 
     except IndexError:
         sys.exit("sgdvb")
