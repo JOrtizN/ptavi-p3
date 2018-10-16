d6dd17bcae92f4f336817e1b086a9e247ffa28f8
Julia Ortiz Naharro <jortizna@zeta25.aulas.gsyc.urjc.es>
2018-10-16 16:56:39 +0200

    corregido karaoke

--- karaoke.py
+++ karaoke.py
@@ -6,29 +6,29 @@ from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSmilHandler
 import sys
 
-try:
 
-    parser = make_parser()
-    Nclase = SmallSmilHandler()
-    parser.setContentHandler(Nclase)
-    parser.parse(open(sys.argv[1]))
-
-    Nlista = Nclase.get_tags()
-    lineaX = ""
+def printlines(Nlista):
     archiv_aux = ""
     for linea in Nlista:
         etiqueta = linea[0]
         atributo = linea[1]
-
         for valor in atributo:
             if atributo[valor] != '':
                etiqueta +=  "\\t" + valor +"=\"" + atributo[valor] + "\""
-
         archiv_aux = archiv_aux + etiqueta + "\n"
-
     print (archiv_aux[:-1])  # para salvar la linea en blanco, mirar como hacerlo mejor
 
+#def makejson(Nlista,)
 
 
-except IndexError:
-    sys.exit("sgdvb")
+if __name__ == "__main__":
+
+    try:
+        parser = make_parser()
+        Nclase = SmallSmilHandler()
+        parser.setContentHandler(Nclase)
+        parser.parse(open(sys.argv[1]))
+        printlines(Nclase.get_tags())
+
+    except IndexError:
+        sys.exit("sgdvb")
